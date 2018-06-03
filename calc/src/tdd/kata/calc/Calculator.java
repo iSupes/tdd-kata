c67972202e7d4a8a41d2cc7f9c3b5bd785815ba9
Khalil <Supes@Supes-LT>
2018-06-03 21:48:40 +0200

    Added custom delimiter code

--- calc/src/tdd/kata/calc/Calculator.java
+++ calc/src/tdd/kata/calc/Calculator.java
@@ -15,6 +15,16 @@ public class Calculator {
         }
         //Set the number delimiter
         String delim = ",";
+        
+        //Checks if a custom delimiter has been specified and uses it if it has
+        if(numbers.length() > 3 && numbers.substring(0,2).equals("//")){
+            delim = numbers.charAt(2)+"";
+            numbers = numbers.substring(4);            
+        } else {
+            //If no custom delimiter has been specified, continues to allow '\n' as well as ','
+            //Replaces new line characters with delimiters to allow '\n' to be used as a delimiter.
+            numbers = numbers.replace("\n", delim);
+        }
         StringTokenizer st = new StringTokenizer(numbers, delim);
         int total = 0;
         try{
@@ -22,7 +32,7 @@ public class Calculator {
             while(st.hasMoreTokens()){
                 total += Integer.parseInt(st.nextToken());
             }
-        //Make sure the string is actually a number and return -1 if there are invalid charachters. 
+        //Make sure the string is actually a number and return -1 if there are invalid characters. 
         //I know invalid input checking isn't necessary, I just couldn't leave it out.
         }catch(NumberFormatException nfe){
             nfe.printStackTrace();
