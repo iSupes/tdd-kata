15a7bea6a1511dcb92c5fe20700ef8acf0607240
Khalil <Supes@Supes-LT>
2018-06-03 21:23:00 +0200

    Added code to do calculation

--- calc/src/tdd/kata/calc/Calculator.java
+++ calc/src/tdd/kata/calc/Calculator.java
@@ -1,17 +1,35 @@
 package tdd.kata.calc;
 
+import java.util.StringTokenizer;
+
 /**
  * TDD Kata Calculator class
  * @author Khalil Hassim
  */
 public class Calculator {
 
-    public static void main(String args[]){
-        
-    }
-    
-    protected int add(String args[]){
-        return -1;
+    protected static int add(String numbers){
+        //Check if the string is empty and return 0 if it is.
+        if(numbers.trim().length() == 0){
+            return 0;
+        }
+        //Set the number delimiter
+        String delim = ",";
+        StringTokenizer st = new StringTokenizer(numbers, delim);
+        int total = 0;
+        try{
+            //Add all the numbers found by the tokenizer together
+            while(st.hasMoreTokens()){
+                total += Integer.parseInt(st.nextToken());
+            }
+        //Make sure the string is actually a number and return -1 if there are invalid charachters. 
+        //I know invalid input checking isn't necessary, I just couldn't leave it out.
+        }catch(NumberFormatException nfe){
+            nfe.printStackTrace();
+            return -1;
+        }
+        //Return added numbers
+        return total;
     }
     
 }
